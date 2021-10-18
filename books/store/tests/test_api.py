5c144a4f75b9a3ba1c8adf6549c2ed223dc0b34a
Dmitry <nor808@yandex.ru>
2021-10-18 13:51:42 +0300

    put post test

--- books/store/tests/test_api.py
+++ books/store/tests/test_api.py
@@ -1,3 +1,5 @@
+import json
+
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -5,12 +7,15 @@ from rest_framework.test import APITestCase
 from store.models import Book
 from store.serializers import BookSerializer
 
+from django.contrib.auth.models import User
+
 
 class BooksApiTestCase(APITestCase):
     def setUp(self):
         self.book_1 = Book.objects.create(name='Test book 1', price=25, author_name='Author 1')
         self.book_2 = Book.objects.create(name='Test book 2', price=55, author_name='Author 5')
         self.book_3 = Book.objects.create(name='Test book Author 1', price=55, author_name='Author 2')
+        self.user = User.objects.create(username='test_username')
 
     def test_get(self):
         url = reverse('book-list')
@@ -32,3 +37,31 @@ class BooksApiTestCase(APITestCase):
         serializer_data = BookSerializer([self.book_1, self.book_3], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
+
+    def test_create(self):
+        self.assertEqual(3, Book.objects.all().count())
+        url = reverse('book-list')
+        data = {
+            "name": "Programming in Python 3",
+            "price": 150,
+            "author_name": "Mark Summerfield",
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.post(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(4, Book.objects.all().count())
+
+    def test_update(self):
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {
+            "name": self.book_1.name,
+            "price": 575,
+            "author_name": self.book_1.author_name,
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.put(url, data=json_data, content_type='application/json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.book_1.refresh_from_db()
+        self.assertEqual(575, self.book_1.price)
