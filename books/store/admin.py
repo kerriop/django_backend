e78b896f7c3858c7399c159c01bf4666716b48da
Dima <nor808@yandex.ru>
2021-10-19 11:17:56 +0300

    7 bookmarks

--- books/store/admin.py
+++ books/store/admin.py
@@ -3,11 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib.admin import ModelAdmin
 
-from store.models import Book
+from store.models import Book, UserBookRelation
 
 
 # admin.site.register(Book)
 
 @admin.register(Book)
 class BookAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(UserBookRelation)
+class UserBookRelationAdmin(ModelAdmin):
     pass
